f85b15332e4b9fa9fd25beea8f0f99425fcec0df
P5421135-2 <H4BI@proton.me>
2025-05-22 11:49:24 +0100

    Fixed warning statements and delay, added comments for further clarity.

--- radarCode/radarCode.ino
+++ radarCode/radarCode.ino
@@ -1,3 +1,7 @@
+　/*
+  The code for the Ultrasonic sensors in the Primary Engineers project. This circuit sits on the backpack and has three ultrasonic sensors to detect the distance of vehicles in three directions, the centre, left and right. The nano scans for the central device on the handlebars and sends over the data read from the ultrasonic sensors, warning statements are included to alert the cyclist of how close approaching vehicles are.
+
+　*/
 #include <ArduinoBLE.h>
 
 // Radar Pins
@@ -21,6 +25,12 @@ BLEFloatCharacteristic leftCharacteristic("19B10002-E8F2-537E-4F6C-D104768A1214"
 BLEFloatCharacteristic rightCharacteristic("19B10003-E8F2-537E-4F6C-D104768A1214", BLERead | BLENotify);
 BLEFloatCharacteristic leftIndicatorCharacteristic("19b10004-e8f2-537e-4f6c-d104768a1214", BLERead | BLEWrite);
 BLEFloatCharacteristic rightIndicatorCharacteristic("19b10005-e8f2-537e-4f6c-d104768a1214", BLERead | BLEWrite);
+
+BLEFloatCharacteristic closeCharacteristic("19B10006-E8F2-537E-4F6C-D104768A1214", BLERead | BLENotify);
+BLEFloatCharacteristic mediumCharacteristic("19B10007-E8F2-537E-4F6C-D104768A1214", BLERead | BLENotify);
+BLEFloatCharacteristic farCharacteristic("19B10008-E8F2-537E-4F6C-D104768A1214", BLERead | BLENotify);
+
+
 
 void setup() {
   Serial.begin(9600);
@@ -55,12 +65,23 @@ void setup() {
   radarService.addCharacteristic(rightCharacteristic);
   radarService.addCharacteristic(leftIndicatorCharacteristic);
   radarService.addCharacteristic(rightIndicatorCharacteristic);
+  radarService.addCharacteristic(closeCharacteristic);
+  radarService.addCharacteristic(mediumCharacteristic);
+  radarService.addCharacteristic(farCharacteristic);
+
+
+
+
 
   BLE.addService(radarService);
 
   centreCharacteristic.writeValue(0.0);
   leftCharacteristic.writeValue(0.0);
   rightCharacteristic.writeValue(0.0);
+
+  closeCharacteristic.writeValue(0.0);
+  mediumCharacteristic.writeValue(0.0);
+  farCharacteristic.writeValue(0.0);
 
   BLE.advertise();
   Serial.println("BLE Radar Peripheral Started");
@@ -86,6 +107,7 @@ void loop() {
     Serial.println(central.address());
 
     while (central.connected()) {
+      　// Function is applied to get the reading for each ultrasonic sensor
       float left = getDistance(RadarLeftTrig, RadarLeftEcho);
       float centre = getDistance(RadarCentreTrig, RadarCentreEcho);
       float right = getDistance(RadarRightTrig, RadarRightEcho);
@@ -100,8 +122,26 @@ void loop() {
       leftCharacteristic.writeValue(left);
       centreCharacteristic.writeValue(centre);
       rightCharacteristic.writeValue(right);
+      // Define thresholds
+      float dangerThreshold = 100.0;
+      float warningThreshold = 300.0;
+      float safeThreshold = 500.0;
 
-      delay(500);  // Adjust delay as needed
+      // Determine proximity state
+      if (left < dangerThreshold || centre < dangerThreshold || right < dangerThreshold) {
+        Serial.println("!!! WARNING: OBJECT VERY CLOSE !!!");
+        closeCharacteristic.writeValue(dangerThreshold);
+      } else if (left < warningThreshold || centre < warningThreshold || right < warningThreshold) {
+        Serial.println("-- CAUTION: Object Approaching --");
+        mediumCharacteristic.writeValue(warningThreshold);
+      } else if (left < safeThreshold || centre < safeThreshold || right < safeThreshold) {
+        Serial.println("Safe: Clear area within range.");
+        farCharacteristic.writeValue(safeThreshold);
+      } else {
+        Serial.println("Clear: No objects detected nearby.");
+      }
+
+      delayMicroseconds(200);  // Adjust delay as needed
 
 
       // if the remote device wrote to the characteristic,
